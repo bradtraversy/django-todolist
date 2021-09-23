cd4f43bb2b8f9ea6004934f02aafebea64fd538f
Manu Johnson <manujohnson2002@gmail.com>
2021-09-23 20:35:23 +0530

    added user in models.py

--- todos/models.py
+++ todos/models.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 from datetime import datetime
+from django.contrib.auth.models import User
 
 from django.db import models
 
+
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
